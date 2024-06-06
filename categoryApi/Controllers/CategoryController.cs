326c4d9f2b05dc9ae9ded1c8a1452342b1dc2337
AbdilatifAli <cabdoo145@hotmail.com>
2024-06-06 03:57:02 +0200

    Initial commit

---
+++ categoryApi/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using categoryApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace categoryApi.Controllers
+
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+     
+        [HttpGet]
+        public List<Models.Category> GetCategories()
+        {
+            return Category.GetCategories();
+        }
+    }
+}
---
+++ categoryApi/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace categoryApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        
+        public static List<Category> GetCategories()
+        {
+           
+            List<Category> categories = new List<Category>();
+
+           
+            categories.Add(new Category { Id = 1, Name = "Premie League",  });
+            categories.Add(new Category { Id = 2, Name = "ChampionsLeague", });
+            categories.Add(new Category { Id = 3, Name = "Spanish League",  });
+            categories.Add(new Category { Id = 4, Name = "EuroCup" });
+
+          
+            return categories;
+        }
+    }
+}
